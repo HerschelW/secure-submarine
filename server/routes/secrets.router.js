baa1200bc1e4a58d9b6f44c704b6e35104ad1e5d
HerschelW <herschelw@gmail.com>
2020-07-01 16:29:27 -0500

    base goals done

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,15 +1,24 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
-        .then(results => res.send(results.rows))
-        .catch(error => {
-            console.log('Error making SELECT for secrets:', error);
-            res.sendStatus(500);
-        });
+const {
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
+//const userStrategy = require("../strategies/user.strategy");
+
+router.get("/", rejectUnauthenticated, (req, res) => {
+  console.log("req.user:", req.user);
+  //const username = req.user.username;
+  const clearance_level = req.user.clearance_level;
+  const queryText = `SELECT * FROM "secret" WHERE $1 >= secrecy_level;`;
+  pool
+    .query(queryText, [clearance_level])
+    .then((results) => res.send(results.rows))
+    .catch((error) => {
+      console.log("Error making SELECT for secrets:", error);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
